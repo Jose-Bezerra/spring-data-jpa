93fca0a0523b105e27e4a74087c60a8493e9dc92
José Bezerra <josbezfi@gmail.com>
2021-10-18 20:49:02 -0300

    Criado uma camada de serviço para salvar objetos cargo, eliminando toda lógica da classe Main.

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,19 +1,22 @@
 package br.com.alura.spring.data;
 
 import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
-	private final CargoRepository repository; //não podemos instanciar uma interface
+	private final CrudCargoService cargoService; //não podemos instanciar uma interface
+	private boolean system = true; //permanecer no sistema
 
 	//Injeção de dependência através do construtor
-	public SpringDataApplication(CargoRepository repository) {
-		this.repository = repository;
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
 	}
 
 	public static void main(String[] args) {
@@ -22,8 +25,21 @@ public class SpringDataApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo cargo = new Cargo();
-		cargo.setDescription("Desenvolvedor de Software");
-		repository.save(cargo);
+		//Pegando os valores do cliente pela console, normalmente seria pelo site
+		Scanner scanner = new Scanner(System.in);
+
+		while (system) {
+			System.out.println("Qual ação deseja executar? ");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+
+			int action = scanner.nextInt();
+			if (action == 1) {
+				cargoService.inicial(scanner);
+			} else {
+				system = false;
+			}
+		}
+
 	}
 }
