1f95def5fd27e30c18885e52a08d700bb1d81676
José Bezerra <josbezfi@gmail.com>
2021-10-18 21:08:06 -0300

    Criado a funcionalidade de atualizar na camada de Serviço.

--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -9,13 +9,32 @@ import java.util.Scanner;
 @Service
 public class CrudCargoService {
     private final CargoRepository cargoRepository;
+    private boolean system = true;
 
     public CrudCargoService(CargoRepository cargoRepository) {
         this.cargoRepository = cargoRepository;
     }
 
     public void inicial(Scanner scanner) {
-        salvar(scanner);
+        while (system) {
+            System.out.println("Qual ação de cargo deseja executar? ");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Salvar");
+            System.out.println("2 - Atualizar");
+
+            int action = scanner.nextInt();
+            switch (action){
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+        }
     }
 
     private void salvar(Scanner scanner) {
@@ -25,6 +44,17 @@ public class CrudCargoService {
         cargo.setDescription(description);
         cargoRepository.save(cargo);
         System.out.println("Salvo com sucesso!");
+    }
 
+    private void atualizar(Scanner scanner) {
+        System.out.println("Informe o Id: ");
+        int id = scanner.nextInt();
+        System.out.println("Descrição do Cargo: ");
+        String description = scanner.next();
+        Cargo cargo = new Cargo();
+        cargo.setId(id);
+        cargo.setDescription(description);
+        cargoRepository.save(cargo);
+        System.out.println("Registro atualizado com sucesso!");
     }
 }
